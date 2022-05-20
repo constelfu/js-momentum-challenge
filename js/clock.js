4b832f92471986ef7d5ae318ec1c032964cde934
constElfu <103916629+constelfu@users.noreply.github.com>
2022-05-20 11:59:47 +0900

    중간과정
    url holder에서 고전중

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,21 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("#clock");
+const yearMonthDay = document.querySelector(".date__text");
 
 function getClock() {
     const date = new Date();
+
+    const years = String(date.getFullYear());
+    const months = String(date.getMonth() + 1);
+    const days = String(date.getDate());
 
     const hours   = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     
-
+    yearMonthDay.innerText = `${years}년 ${months}월 ${days}일`;
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
 getClock();
 setInterval(getClock, 1000);    // 일정 시간마다 실행
 //setTimeout(sayHello, 5000);   // 시간지연함수
---
+++ js/url-holder.js
@@ -0,0 +1,57 @@
+const urlHolder = document.querySelector(".url-holder__form");
+const urlList = document.querySelector(".url-holder__list");
+const urlInput = document.querySelector(".url-holder__input");
+
+const URL_KEY = "urls";
+
+let urlArray = [];
+
+function handleUrlSummit(event) {
+    event.preventDefault();
+    const newUrlArray = urlInput.value;
+    urlInput.value = "";
+    const newUrlObj = {
+        text: newUrlArray,
+        id: Date.now(),
+    };
+    urlArray.push(newUrlObj);
+    paintUrl(newUrlObj);
+    saveUrlArray();
+}
+
+function paintUrl(newUrl) {
+    const li = document.createElement("li");
+    li.id = newUrl.id;
+    const span = document.createElement("span");
+    span.innerText = newUrl.text;
+    const button = document.createElement("button");
+    button.innerText = "Delete";
+    button.addEventListener("click", deleteUrl);
+    
+    li.appendChild(span);
+    li.appendChild(button);
+
+    urlList.appendChild(li);
+}
+
+function deleteUrl(event) {
+    const li = event.target.parentElement;
+    li.remove();
+    urlArray = urlArray.filter(urlArray => urlArray.id !== parseInt(li.id));
+    saveUrlArray();        // 원래 있던 UrlArray 전역변수 배열
+    
+}
+
+function saveUrlArray() {
+    localStorage.setItem(URL_KEY, JSON.stringify(urlArray));
+}
+
+urlHolder.addEventListener("submit", handleUrlSummit);
+
+const savedUrlArray = localStorage.getItem(URL_KEY);
+
+if (savedUrlArray !== null) {
+    const parsedUrlArray = JSON.parse(savedUrlArray);
+    urlArray = parsedUrlArray;
+    parsedUrlArray.forEach(paintUrl);                                  // 배열의 각 요소에 대해서 해당 함수를 수행                                                                     // 수행되는 함수에 event매개변수를 넣으면 해당 배열의 각 요소가 event매개변수로 들어가서 수행됨
+}                                                                    // 자체 함수 작성기능도 있다 arrow function =>
